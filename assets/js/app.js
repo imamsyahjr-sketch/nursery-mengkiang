f30caac89ad8a5dcaf79e63d3125e480c6e12488
imamsyahjr-sketch <imamsyahjr@gmail.com>
2026-02-06 21:36:13 +0700

    Add files via upload

---
+++ assets/js/app.js
@@ -0,0 +1,44 @@
+const sidebar = document.getElementById("sidebar");
+const overlay = document.getElementById("overlay");
+const toggleBtn = document.getElementById("toggleBtn");
+
+
+
+/* SIDEBAR */
+toggleBtn.onclick = () => {
+    sidebar.classList.toggle("active");
+    overlay.classList.toggle("active");
+};
+overlay.onclick = () => {
+    sidebar.classList.remove("active");
+    overlay.classList.remove("active");
+};
+
+/* THEME */
+const themeToggle = document.getElementById("themeToggle");
+
+if (localStorage.getItem("theme") === "dark") {
+    document.body.classList.add("dark");
+    themeToggle.checked = true;
+}
+
+themeToggle.onchange = () => {
+    document.body.classList.toggle("dark");
+    localStorage.setItem(
+        "theme",
+        document.body.classList.contains("dark") ? "dark" : "light"
+    );
+};
+
+if (localStorage.getItem("theme") === "dark") {
+    document.body.classList.add("dark");
+}
+
+/* SLIDER */
+let slides = document.querySelectorAll(".slide");
+let index = 0;
+setInterval(() => {
+    slides[index].classList.remove("active");
+    index = (index + 1) % slides.length;
+    slides[index].classList.add("active");
+}, 3500);
